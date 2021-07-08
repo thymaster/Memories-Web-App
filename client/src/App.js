88fe83b0200a7bc133cfede6483caf189d99cb92
Anish Sarkar <anish_pg@ece.nits.ac.in>
2021-07-08 23:03:04 +0530

    deployed and working

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Auth } from "./components/Auth/Auth";
+import { Home } from "./components/Home/Home";
+
+const App = () => (
+  <BrowserRouter>
+    <Container maxWidth="lg">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
+    </Container>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ client/src/components/Auth/Auth.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Auth = () => {
+  const state = null;
+  return <div>AUTH</div>;
+};
